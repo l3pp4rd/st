b543beff65f6fb05f30bb3d98d6a6bd589d96c4b
gedi <gediminas.morkevicius@gmail.com>
2014-05-11 16:41:56 +0300

    configurations, solarized dark, inconsolata fonts

--- config.mk
+++ config.mk
@@ -7,8 +7,8 @@ VERSION = 0.5
 PREFIX = /usr/local
 MANPREFIX = ${PREFIX}/share/man
 
-X11INC = /usr/X11R6/include
-X11LIB = /usr/X11R6/lib
+X11INC = /usr/include/X11
+X11LIB = /usr/lib/X11
 
 # includes and libs
 INCS = -I. -I/usr/include -I${X11INC} \
