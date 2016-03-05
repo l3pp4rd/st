7a5d83523cbcf136881cfb9e977e840bbf0b30ff
gedi <gediminas.morkevicius@gmail.com>
2016-03-06 00:25:22 +0200

    since inconsolata has bold support now, hintstyle change, should be BC

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char font[] = "Inconsolata:pixelsize=19:lcdfilter=lcddefault:hintstyle=hintfull:hinting=true:rgba=rgb:antialias=true:autohint=false";
+static char font[] = "Inconsolata:pixelsize=19:lcdfilter=lcddefault:hintstyle=hintnone:rgba=rgb:antialias=true:autohint=false";
 static int borderpx = 0;
 static char shell[] = "/bin/zsh";
 
